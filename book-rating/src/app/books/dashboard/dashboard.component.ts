c81528ee9c15f6d860db7a38f5f38eb6de8a8860
Ferdinand Malcher <ferdinand@malcher.media>
2022-01-18 14:27:27 +0100

    Dashboard: Service nutzen

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from '../shared/book';
+import { BookRatingService } from '../shared/book-rating.service';
 
 
 @Component({
@@ -11,7 +12,7 @@ export class DashboardComponent implements OnInit {
 
   books: Book[] = [];
 
-  constructor() {
+  constructor(private rs: BookRatingService) {
     this.books = [
       {
         isbn: '123',
@@ -34,11 +35,19 @@ export class DashboardComponent implements OnInit {
   }
 
   onRateUp(book: Book) {
-    console.log('UP', book);
+    const ratedBook = this.rs.rateUp(book);
+    this.updateList(ratedBook);
+
   }
 
   onRateDown(book: Book) {
-    console.log('DOWN', book);
+    const ratedBook = this.rs.rateDown(book);
+    this.updateList(ratedBook);
+  }
+
+  private updateList(ratedBook: Book) {
+    this.books = this.books
+      .map(b => b.isbn === ratedBook.isbn ? ratedBook : b);
   }
 
 }
