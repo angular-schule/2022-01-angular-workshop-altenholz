79b120cf62148beaf0c8e97664f447db02a1c976
Ferdinand Malcher <ferdinand@malcher.media>
2022-01-19 10:24:19 +0100

    Routing: Parameter

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'br-book-details',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {
+    // Synchroner Weg / PULL
+    // const isbn = this.route.snapshot.paramMap.get('isbn'); // path: 'books/:isbn'
+
+    // Asynchroner Weg / PUSH
+    this.route.paramMap.subscribe(params => {
+      const isbn = params.get('isbn');
+      console.log(isbn);
+    });
+  }
 
   ngOnInit(): void {
   }
