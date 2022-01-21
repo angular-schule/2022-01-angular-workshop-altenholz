1ad26d259dc7df6c43443922ef61050a002b6f91
Ferdinand Malcher <ferdinand@malcher.media>
2022-01-21 15:08:41 +0100

    Typeahead-Suche

--- book-rating/src/app/books/book-search/book-search.component.ts
+++ book-rating/src/app/books/book-search/book-search.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, filter, Observable, of, switchMap, tap } from 'rxjs';
+import { Book } from '../shared/book';
+import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'br-book-search',
@@ -9,10 +12,38 @@ import { FormControl } from '@angular/forms';
 export class BookSearchComponent implements OnInit {
 
   searchControl = new FormControl('');
+  result$: Observable<Book[]>;
 
-  constructor() {
-    this.searchControl.valueChanges
-      .subscribe(e => console.log(e));
+  loading = false;
+
+  constructor(private bs: BookStoreService) {
+    const term$: Observable<string> = this.searchControl.valueChanges
+
+    this.result$ = term$.pipe(
+      debounceTime(1000),
+      filter(term => term.length >= 3 || term.length === 0),
+      distinctUntilChanged(),
+
+      switchMap(term => {
+        if (term.length === 0) {
+          return of([]);
+        } else {
+          return this.bs.search(term);
+        }
+      }),
+      tap(() => this.loading = false),
+    );
+
+
+
+
+    // Suchbegriff mindestens 3 Zeichen lang (RxJS)
+    // nicht zu viele Anfragen hintereinander
+    // keine zwei gleichen Begriffe direkt nacheinander //
+    // BookStoreService.search()
+    // anzeigen (ganz einfach)
+    // AsyncPipe
+    // Zusatz: Ladeanzeige //
   }
 
   ngOnInit(): void {
